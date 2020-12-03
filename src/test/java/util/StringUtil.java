b5d3b6f2f93eeddb1981aa72cbacd4fa3c6b2bff
Kirill Savchuk <kirillss1998@gmail.com>
2020-12-03 17:30:13 +0100

    Adjusted StringUtil to support toList transformation

--- src/test/java/util/StringUtil.java
+++ src/test/java/util/StringUtil.java
@@ -1,11 +1,29 @@
 package util;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StringUtil {
 
 	private final static String DEFAULT_STRING_DIVIDER = " ";
+
+	public static List<Integer> toIntList(String str) {
+		return Arrays.stream(toIntArray(str)).boxed().collect(Collectors.toList());
+	}
+
+	public static List<Long> toLongList(String str) {
+		return Arrays.stream(toLongArray(str)).boxed().collect(Collectors.toList());
+	}
+
+	public static List<Double> toDoubleList(String str) {
+		return Arrays.stream(toDoubleArray(str)).boxed().collect(Collectors.toList());
+	}
+
+	public static List<String> toStringList(String str) {
+		return Arrays.stream(toStringArray(str)).collect(Collectors.toList());
+	}
 
 	public static int[] toIntArray(String str) {
 		return split(str).mapToInt(Integer::valueOf).toArray();
