cff803e2837352fb0918eb7a33527650194fb0a1
Kirill Savchuk <kirillss1998@gmail.com>
2020-12-03 17:08:09 +0100

    StringsUtil for transforming String examples into required input/output data types

---
+++ src/test/java/util/StringUtil.java
@@ -0,0 +1,30 @@
+package util;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class StringUtil {
+
+	private final static String DEFAULT_STRING_DIVIDER = " ";
+
+	public static int[] toIntArray(String str) {
+		return split(str).mapToInt(Integer::valueOf).toArray();
+	}
+
+	public static long[] toLongArray(String str) {
+		return split(str).mapToLong(Long::valueOf).toArray();
+	}
+
+	public static double[] toDoubleArray(String str) {
+		return split(str).mapToDouble(Double::valueOf).toArray();
+	}
+
+	public static String[] toStringArray(String str) {
+		return split(str).toArray(String[]::new);
+	}
+
+	private static Stream<String> split(String str) {
+		return Arrays.stream(str.split(DEFAULT_STRING_DIVIDER));
+	}
+
+}
